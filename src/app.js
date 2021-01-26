6f0685c6f4f6b60fe5ded00043befb882669c8ac
Nick Charbonneau <nick.charby@gmail.com>
2021-01-26 12:09:55 -0800

    Add docs, cleanup

--- src/app.js
+++ src/app.js
@@ -2,15 +2,11 @@ const express = require('express');
 const path = require('path');
 const helmet = require('helmet');
 const xss = require('xss-clean');
-// const mongoSanitize = require('express-mongo-sanitize');
 const compression = require('compression');
 const cors = require('cors');
-// const passport = require('passport');
 const httpStatus = require('http-status');
 const config = require('./config/config');
 const morgan = require('./config/morgan');
-// const { jwtStrategy } = require('./config/passport');
-// const { authLimiter } = require('./middlewares/rateLimiter');
 const routes = require('./routes/root');
 const { errorConverter, errorHandler } = require('./middlewares/error');
 const ApiError = require('./utils/ApiError');
@@ -33,7 +29,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // sanitize request data
 app.use(xss());
-// app.use(mongoSanitize());
 
 // gzip compression
 app.use(compression());
@@ -41,15 +36,6 @@ app.use(compression());
 // enable cors
 app.use(cors());
 app.options('*', cors());
-
-// jwt authentication
-// app.use(passport.initialize());
-// passport.use('jwt', jwtStrategy);
-
-// limit repeated failed requests to auth endpoints
-// if (config.env === 'production') {
-//   app.use('/v1/auth', authLimiter);
-// }
 
 // root routes
 app.use('/', routes);
